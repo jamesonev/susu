c21d166dfc4e7716e193ab0d1c13fa04a01dc959
Jameson Evans <6jh7ln@gmail.com>
2019-11-02 20:35:09 -0700

    added ABI

--- hotpot-frontend/scripts/main.js
+++ hotpot-frontend/scripts/main.js
@@ -2,3 +2,8 @@ const Eth = require('ethjs');
 const eth = new Eth(new Eth.HttpProvider('https://rinkeby.infura.io'));
 
 const hotpot = eth.contract(tokenABI).at('0x6e0E0e02377Bc1d90E8a7c21f12BA385C2C35f78');
+
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('../../build/hotpot.json', 'utf8'));
+const ABI = obj['abi'];n
+console.log(ABI);
