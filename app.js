087d4859ff31671cc30aa197032e89e0115e4a15
Jameson Evans <6jh7ln@gmail.com>
2019-11-02 19:39:43 -0700

    imported frontend from sketch; no linking

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('hotpot-frontend'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
