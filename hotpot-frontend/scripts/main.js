7d956c645bd752406e7801586c67f1d44a6e74d7
Preston Evans <pbevans1@crimson.ua.edu>
2019-11-02 20:02:59 -0700

    main.js

--- hotpot-frontend/scripts/main.js
+++ hotpot-frontend/scripts/main.js
@@ -1,2 +1,4 @@
 const Eth = require('ethjs');
 const eth = new Eth(new Eth.HttpProvider('https://rinkeby.infura.io'));
+
+const hotpot = eth.contract(tokenABI).at('0x6e0E0e02377Bc1d90E8a7c21f12BA385C2C35f78');
